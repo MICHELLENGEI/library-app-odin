0b1519615b0f7b8e434db3f76008e243cc7ce51c
MICHELLLENGEI <michellengei20@gmail.com>
2024-02-29 13:46:31 +0300

    added fun for adding book data to array

--- js/main.js
+++ js/main.js
@@ -1,29 +1,47 @@
 const myLibrary = new Array();
 
-function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
 
-}
 
 function showModal(params) {
     const newBookButton = document.querySelector('.newBook');
     const modal = document.querySelector('.modal');
     const closeModalButton = document.querySelector('.close');
 
-    newBookButton.addEventListener('click', function() {
+    newBookButton.addEventListener('click', function () {
         modal.style.display = "block";
     });
-    closeModalButton.addEventListener('click', function() {
+    closeModalButton.addEventListener('click', function () {
         modal.style.display = "none";
     });
 }
 showModal();
-const book = new Book('Keeping up', 'Ngei', 294, 'read')
-myLibrary.push(book);
-console.log(myLibrary);
+
+modal.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.querySelector('input[name="read"]:checked').value;
+
+
+    function Book(title, author, pages, status) {
+        title = title;
+        author = author;
+        pages = pages;
+        status = status;
+
+    }
+    myLibrary.push(book);
+    console.log(myLibrary);
+
+    displayBooks();
+
+    // Reset form fields
+    bookForm.reset();
+
+    // Close the modal
+    modal.style.display = "none";
+});
 
 for (let i = 0; i < myLibrary.length; i++) {
     console.log(myLibrary[i])
