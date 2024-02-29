1b684509b46f8e583f18a8b67d89c04ed512f963
MICHELLLENGEI <michellengei20@gmail.com>
2024-02-29 13:28:41 +0300

    added fun for show and hide form

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
-const  myLibrary = new Array();
+const myLibrary = new Array();
 
-function Book(title , author, pages, status) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.status = status;
-    
-}
-const book = new Book('Keeping up','Ngei',294, 'read')
-myLibrary.push(book);
-console.log(myLibrary); 
 
-for(let i=0; i < myLibrary.length; i++){
+}
+
+function showModal(params) {
+    const newBookButton = document.querySelector('.newBook');
+    const modal = document.querySelector('.modal');
+    const closeModalButton = document.querySelector('.close');
+
+    newBookButton.addEventListener('click', function() {
+        modal.style.display = "block";
+    });
+    closeModalButton.addEventListener('click', function() {
+        modal.style.display = "none";
+    });
+}
+showModal();
+const book = new Book('Keeping up', 'Ngei', 294, 'read')
+myLibrary.push(book);
+console.log(myLibrary);
+
+for (let i = 0; i < myLibrary.length; i++) {
     console.log(myLibrary[i])
 }
